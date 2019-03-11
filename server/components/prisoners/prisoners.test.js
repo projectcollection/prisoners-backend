953099878edf54f0757e8bad3124ce80edff5396
Trai Lynne <trailynne@gmail.com>
2019-03-11 18:36:07 -0400

    Define CREATE method Accepts an object of the prisons information Returns record from DB

--- server/components/prisoners/prisoners.test.js
+++ server/components/prisoners/prisoners.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
 const server = require('./index');
-const db = require('./prisonersModel');
+
 
 describe('Prisoners', () => {
     describe('Model', () => {
-
+        
     });
 
     describe('Router', () => {
--- server/components/prisons/prisonsModel.js
+++ server/components/prisons/prisonsModel.js
@@ -1,6 +1,22 @@
 const db = require('../../../db/dbConfig');
 
+const readTable = () => db('prisons');
+const clearTestTable = () => db('prisons').truncate();
+
+const findBy = filter => db('prisons').where(filter);
+
+
+const create = async prison => {
+    const [id] = await db('prisons').insert(prison);
+
+    return findBy({
+        id
+    }).first();
+}
 
 module.exports = {
-
+    readTable,
+    clearTestTable,
+    findBy,
+    create
 }
