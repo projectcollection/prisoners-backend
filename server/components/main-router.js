0dfeb18f9b213b311b612e5bfad88449584a9e5d
Trai Lynne <trailynne@gmail.com>
2019-03-11 16:31:51 -0400

    Define generic sub-routers Copied code from main-router and refactored for each file Imported sub routers to main router and defined routes accordingly

--- server/components/main-router.js
+++ server/components/main-router.js
@@ -6,9 +6,18 @@ const mainRouter = express.Router();
 
 
 // Sub Routers
+const authRoutes = require('./auth');
+const prisonRoutes = require('./prisons');
+const prisonerRoutes = require('./prisoners');
+const skillRoutes = require('./skills');
 
 
 // Routes
+mainRouter.use('/auth', authRoutes);
+mainRouter.use('/prisons', prisonRoutes);
+mainRouter.use('/prisoners', prisonerRoutes);
+mainRouter.use('/skills', skillRoutes);
+
 mainRouter.use('/', (req, res) => res.send('Welcome to the Main API'));
 
 // Export
