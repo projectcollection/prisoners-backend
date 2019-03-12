f5a7fa7d42d988f24a2cd5289ff5e24c98c9c17c
Trai Lynne <trailynne@gmail.com>
2019-03-12 15:44:55 -0400

    Add Basic model functions to Skills and Prisoners Copied code from prisonsModel and refactored

--- server/components/skills/skillsModel.js
+++ server/components/skills/skillsModel.js
@@ -1,6 +1,48 @@
 const db = require('../../../db/dbConfig');
 
+const readTable = () => db('skills');
+const clearTestTable = () => db('skills').truncate();
+
+const findBy = filter => db('skills').where(filter);
+
+
+const createSkill = async skill => {
+    const [id] = await db('skills').insert(skill);
+
+    return findBy({
+        id
+    }).first();
+}
+
+const readSkills = () => db('skills');
+
+
+const readSkill = id => db('skills').where({
+    id
+}).first();
+
+const updateSkill = async (id, updates) => {
+    await db('skills').where({
+        id
+    }).update(updates);
+
+    return findBy({
+        id
+    }).first();
+};
+
+
+const destroySkill = id => db('skills').where({
+    id
+}).del();
 
 module.exports = {
-
+    readTable,
+    clearTestTable,
+    findBy,
+    createSkill,
+    readSkills,
+    readSkill,
+    updateSkill,
+    destroySkill
 }
