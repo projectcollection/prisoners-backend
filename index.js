fe2a87d5b1abff1258280f2f802f5e94b97dac61
Aleczander Jordan <alec.jordan.cs@gmail.com>
2019-03-13 12:48:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,4 @@ const server = require('./server/server');
 const port = process.env.PORT || 5000
 
 // Listener
-server.listen(process.env.PORT || 5000
-, () => console.log(`Server listening on port ${port}`));
+server.listen(port, () => {console.log(`Server listening on port ${port}`)});
