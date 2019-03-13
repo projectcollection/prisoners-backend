815d6d598cfa22b0f35ec9e6b90ebcbdaba308de
Aleczander Jordan <alec.jordan.cs@gmail.com>
2019-03-13 12:36:54 -0400

    Small change, testing heroku deploy

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const server = require('./server/server');
 const port = process.env.PORT || 5000
 
 // Listener
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+server.listen(process.env.PORT || 5000
+, () => console.log(`Server listening on port ${port}`));
