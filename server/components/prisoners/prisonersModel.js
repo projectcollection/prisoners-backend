312e7441edfca253bafb9744e0104d865f63e46f
SabrinaGear <sabrinagear.ft@gmail.com>
2019-03-22 21:42:46 -0500

    let's try this, removing second join on prisonsers-skills route

--- server/components/prisoners/prisonersModel.js
+++ server/components/prisoners/prisonersModel.js
@@ -1,85 +1,100 @@
-const db = require('../../../db/dbConfig');
+const db = require("../../../db/dbConfig");
 
-const readTable = () => db('prisoners');
-const clearTestTable = () => db('prisoners').truncate();
+const readTable = () => db("prisoners");
+const clearTestTable = () => db("prisoners").truncate();
 
-const findBy = filter => db('prisoners').where(filter);
-
+const findBy = filter => db("prisoners").where(filter);
 
 const createPrisoner = async prisoner => {
-    const [id] = await db('prisoners').insert(prisoner);
+  const [id] = await db("prisoners").insert(prisoner);
 
-    return findBy({
-        id
-    }).first();
-}
-
-const addSkills = (prisoner_id, skill_id) => db('prisonerSkills').insert({
-    prisoner_id,
-    skill_id
-});
-
-const readPrisoners = () => db('prisoners').join('prisons', 'prisoners.prison_id', '=', 'prisons.id').select('prisoners.*', 'prisons.location');
-
-const readPrisoner = id => db('prisoners').where({id}).first();
-
-const readPrisonerSkills = id => db('prisonerSkills')
-    .join('prisoners', 'prisoners.id', '=', 'prisonerSkills.prisoner_id')
-    .join('skills', 'skills.id', '=', 'prisonerSkills.skill_id')
-    .where({
-        prisoner_id: id
-    })
-    .select('skills.name');
-
-const readPrison = id => db('prisons').select('location', 'population', 'zipcode').where({id}).first();
-
-const updatePrisoner = async (id, updates) => {
-    await db('prisoners').where({
-        id
-    }).update(updates);
-
-    return findBy({
-        id
-    }).first();
+  return findBy({
+    id
+  }).first();
 };
 
+const addSkills = (prisoner_id, skill_id) =>
+  db("prisonerSkills").insert({
+    prisoner_id,
+    skill_id
+  });
+
+const readPrisoners = () =>
+  db("prisoners")
+    .join("prisons", "prisoners.prison_id", "=", "prisons.id")
+    .select("prisoners.*", "prisons.location");
+
+const readPrisoner = id =>
+  db("prisoners")
+    .where({ id })
+    .first();
+
+const readPrisonerSkills = id =>
+  db("prisonerSkills")
+    .join("prisoners", "prisoners.id", "=", "prisonerSkills.prisoner_id")
+    .where({
+      prisoner_id: id
+    })
+    .select("skills.name");
+
+const readPrison = id =>
+  db("prisons")
+    .select("location", "population", "zipcode")
+    .where({ id })
+    .first();
+
+const updatePrisoner = async (id, updates) => {
+  await db("prisoners")
+    .where({
+      id
+    })
+    .update(updates);
+
+  return findBy({
+    id
+  }).first();
+};
 
 const destroyPrisoner = async id => {
-    await db('prisonerSkills').where({
-        prisoner_id: id
-    }).del();
-    
-    console.log('done1');
-    
-    await db('prisoners').where({
-        id
-    }).del();
-    
-    console.log('done2');
+  await db("prisonerSkills")
+    .where({
+      prisoner_id: id
+    })
+    .del();
+
+  console.log("done1");
+
+  await db("prisoners")
+    .where({
+      id
+    })
+    .del();
+
+  console.log("done2");
 };
 
 const destroyPrisonerSkill = (id, skill_id) => {
-    db('prisonerSkills')
+  db("prisonerSkills")
     .where({
-        prisoner_id: id
+      prisoner_id: id
     })
     .where({
-        skill_id
+      skill_id
     })
     .del();
-}
+};
 
 module.exports = {
-    readTable,
-    clearTestTable,
-    findBy,
-    createPrisoner,
-    addSkills,
-    readPrisoners,
-    readPrisoner,
-    readPrisonerSkills,
-    readPrison,
-    updatePrisoner,
-    destroyPrisoner,
-    destroyPrisonerSkill
-}
+  readTable,
+  clearTestTable,
+  findBy,
+  createPrisoner,
+  addSkills,
+  readPrisoners,
+  readPrisoner,
+  readPrisonerSkills,
+  readPrison,
+  updatePrisoner,
+  destroyPrisoner,
+  destroyPrisonerSkill
+};
